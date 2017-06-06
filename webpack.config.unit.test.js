fce29f93bd5297572942cec73b99b9f8ab8f3130
Artur Sum <sum.artur@gmail.com>
2017-06-06 11:55:10 +0200

    setup test frameworks done

---
+++ webpack.config.unit.test.js
@@ -0,0 +1,57 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+
+module.exports = {
+
+    entry: './tests/unit/entry.js',
+
+    output: {
+        filename: 'unitTest.js',
+        path: __dirname + "/dist",
+    },
+
+
+    module : {
+
+        rules: [
+
+            {
+                test: /\.(js|jsx)$/,
+
+                exclude: /node_modules/,
+
+                use: [{
+
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["react"]
+                    }
+
+                }]
+            },
+
+
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: "css-loader"
+                })
+            },
+
+            {
+
+                test: /\.(woff|eot|svg|ttf|woff2)$/,
+                use: 'url-loader'
+
+            }
+
+        ]
+
+    },
+
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
+
+};
